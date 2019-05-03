828b64db4a2b54a109149ff50aefe28a3497509d
Andressa <andressamp90@gmail.com>
2019-05-03 20:50:33 -0300

    Projeto principal 2

--- js/pedido.js
+++ js/pedido.js
@@ -1,15 +1,15 @@
 var usersCli = document.getElementById('usersCli');
-var nameInput = document.getElementById('nameInput');
-var telInput = document.getElementById('telInput');
-var emailInput = document.getElementById('emailInput');
-var cidadeInput = document.getElementById('cidadeInput');
-var estadoInput = document.getElementById('estadoInput');
-var addButton = document.getElementById('addButton');
 var usersProd = document.getElementById('usersProd');
+var cliInput = document.getElementById('cliInput');
+var prodInput = document.getElementById('prodInput');
+var ageInput = document.getElementById('ageInput');
+var totalInput = document.getElementById('totalInput');
+
+
 
 // Ao clicar no botão
 addButton.addEventListener('click', function () {
-    create(nameInput.value, telInput.value, emailInput.value, cidadeInput.value, estadoInput.value);
+    create(cliInput.value, prodInput.value, ageInput.value, totalInput.value);
 });
 
 // Ao mudar o produto
@@ -19,17 +19,17 @@ function teste() {
 };
 
 // Função para criar um registro no Firebase
-function create(name, tel, email, cidade, estado) {
+function create(cli, prod, age, total) {
     var data = {
-        name: name,
-        tel: tel,
-        email: email,
-        cidade: cidade,
-        estado: estado
+        cli: cli,
+        prod: prod,
+        age: age,
+        total: total
+       
 
     };
 
-    return firebase.database().ref().child('cliente').push(data);
+    return firebase.database().ref().child('pedido').push(data);
 }
 firebase.database().ref('cliente').on('value', function (snapshot) {
     snapshot.forEach(function (item) {
@@ -43,6 +43,14 @@ firebase.database().ref('users').on('value', function (snapshot) {
     snapshot.forEach(function (item) {
         var option = document.createElement('option');
         option.appendChild(document.createTextNode(item.val().name));
+        usersProd.appendChild(option);
+    });
+});
+firebase.database().ref('pedido').on('value', function (snapshot) {
+    snapshot.forEach(function (item) {
+        var option = document.createElement('option');
+        option.appendChild(document.createTextNode(item.val().cli + ': ' + item.val().prod + ': ' + item.val().age + ': ' + item.val().total));
+        usersCli.appendChild(option);
         usersProd.appendChild(option);
     });
 });
